8f4f51fd11b2754236411a6373909805e039dba0
frevirard <frevirard@gmail.Com>
2024-10-11 17:10:11 +0200

    "metrics"

--- src/main/java/com/example/creato/objets/metrics/metrics.java
+++ src/main/java/com/example/creato/objets/metrics/metrics.java
@@ -24,5 +24,6 @@ public class Metrics {
     public Long projetsOperation;
     public Long projetsPilotage;
     public Long totalProjet;
+    public Long moyenneInterco;
     public Long pourcentageNouveau;
 }
